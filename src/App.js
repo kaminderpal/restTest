62fc9e229e8d7e2a89217744c2f41ff6afc4a5a1
Kamy <kaminderpal@yahoo.com>
2020-12-07 23:24:25 -0500

    set up header and main section of the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <div></div>;
-}
+import React from 'react';
+import Header from './Header';
+import Main from './Main';
+
+const App = () => {
+  return (
+    <div>
+      <Header title="Bench Test" />
+      <Main />
+    </div>
+  );
+};
 
 export default App;
