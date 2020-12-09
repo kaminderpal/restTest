2b04b282e7e14c4b1ffe548c6dcf4a94ad33f1ef
Kamy <kaminderpal@yahoo.com>
2020-12-08 21:46:03 -0500

    update datatable to improve performace

--- src/Datatable/Datatable.js
+++ src/Datatable/Datatable.js
@@ -1,52 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styles from './Datatable.module.css';
-import useFetch from '../Utils/useFetch';
 import Row from './Row';
 import { getDate, getTotalAmount } from '../Utils/helpers';
 
-const Datatable = () => {
-  const [transactions, setTransactions] = useState([]);
-  const [totalCount, setTotalCount] = useState(0);
-
-  const { data, getData } = useFetch({
-    result: null,
-    isLoading: false,
-    error: false,
-    page: 1,
-  });
-
-  /**
-   * Effect that will setup the count to retreive the next page.
-   */
-  useEffect(() => {
-    if (data.result && data.page === 1) {
-      setTotalCount(
-        Math.ceil(data.result.totalCount / data.result.transactions.length)
-      );
-    }
-  }, [data.result, data.page]);
-
-  /**
-   * Effect that will set the transactions.
-   */
-  useEffect(() => {
-    if (data.result) {
-      setTransactions((t) => [...t, ...data.result.transactions]);
-    }
-  }, [data.result]);
-
-  /**
-   * Effect that will help to get data from server with different page.
-   */
-  useEffect(() => {
-    if (data.result && data.page < totalCount) {
-      getData(data.page + 1);
-    }
-  }, [data.result, totalCount, data.page, getData]);
-
-  if (data.isLoading) return 'Transactions are loading...';
-  if (data.error) return 'Error Occurred while loading the transactions';
-
+const Datatable = ({ transactions }) => {
   return (
     <table className={styles.table}>
       <thead>
@@ -54,22 +11,19 @@ const Datatable = () => {
           <th>Date</th>
           <th>Company</th>
           <th>Account</th>
-          <th>{`$${
-            transactions.length ? getTotalAmount(transactions) : 'Value'
-          }`}</th>
+          <th>{`$${getTotalAmount(transactions)}`}</th>
         </tr>
       </thead>
       <tbody className={styles.tbody}>
-        {transactions.length > 0 &&
-          transactions.map((transaction, i) => (
-            <Row
-              key={i}
-              date={getDate(transaction.Date)}
-              amount={transaction.Amount}
-              account={transaction.Ledger}
-              company={transaction.Company}
-            />
-          ))}
+        {transactions.map((transaction, i) => (
+          <Row
+            key={i}
+            date={getDate(transaction.Date)}
+            amount={transaction.Amount}
+            account={transaction.Ledger}
+            company={transaction.Company}
+          />
+        ))}
       </tbody>
     </table>
   );
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Datatable from '../Datatable';
 import styles from './Main.module.css';
+import useFetch from '../Utils/useFetch';
 
 const Main = () => {
+  const [transactions, setTransactions] = useState([]);
+  const [maxPage, setMaxPage] = useState(0);
+
+  /**
+   * Fetch hook to get data from server.
+   */
+  const { data, getData } = useFetch({
+    result: null,
+    isLoading: false,
+    error: false,
+    page: 1,
+  });
+
+  /**
+   * Effect that will setup the maxpage
+   * MaxPage means no. of times we gonna make fetch call.
+   */
+  useEffect(() => {
+    if (data.result && data.page === 1) {
+      setMaxPage(
+        Math.ceil(data.result.totalCount / data.result.transactions.length)
+      );
+    }
+  }, [data.result, data.page]);
+
+  /**
+   * Effect that will set the transactions.
+   */
+  useEffect(() => {
+    if (data.result) {
+      setTransactions((t) => [...t, ...data.result.transactions]);
+    }
+  }, [data.result]);
+
+  /**
+   * Effect that will help to get data from server with different page.
+   */
+  useEffect(() => {
+    if (data.result && data.page < maxPage) {
+      getData(data.page + 1);
+    }
+  }, [data.result, maxPage, data.page, getData]);
+
   return (
     <div className={styles.main}>
-      <Datatable />
+      {data.isLoading && 'Transactions are loading...'}
+      {data.error && 'Error Occurred while loading the transactions'}
+      {transactions.length > 0 && <Datatable transactions={transactions} />}
     </div>
   );
 };
